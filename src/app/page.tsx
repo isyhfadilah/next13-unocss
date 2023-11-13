b7505c46d21ad6b77f1c574b40cae23b0f2d4ae5
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-11-13 10:54:10 +0700

    fix: eslint plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <span className="text-red">Installed</span>
+      <span className="text-red font-bold">Installed</span>
     </main>
   )
 }
